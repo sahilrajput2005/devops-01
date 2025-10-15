afb7ec970fd9df4bd615ef4e86ac6d455e355e4f
sahilrajput2005 <kumarsahilsk815@gmail.com>
2025-10-15 19:30:29 +0530

    commited

--- backend/urls.py
+++ backend/urls.py
@@ -13,9 +13,9 @@ kl
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including 
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('
 """
 from django.contrib import admin
 from django.urls import path
