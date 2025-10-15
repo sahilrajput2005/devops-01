cd84c17f4e8ff5944cfbcaccd0f2431b4e76f46f
sahilrajput2005 <kumarsahilsk815@gmail.com>
2025-10-15 17:48:55 +0530

    All updated

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
+lll
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
