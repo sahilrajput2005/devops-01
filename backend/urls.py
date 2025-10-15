6d0e16d3448e692310b5e04334fdd5fe4458051a
sahilrajput2005 <kumarsahilsk815@gmail.com>
2025-10-15 18:35:53 +0530

    ommit

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,8 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
 lll
+
+kl
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
